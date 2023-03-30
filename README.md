2dbaa5794831de1df187c8791c37fc39342e70f3
Daniel Quinn <danquinndev@gmail.com>
2023-03-29 19:54:49 -0700

    Working on Vue version

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# browsegpt
-Chrome extension to automate your browser with artificial intelligence
+# BrowseGPT
+
+https://browsegpt.ai
+
+Chrome extension to automate your browser with artificial intelligence.
+
+## Local development setup instructions
+
+Start by installing Vue in the /extension folder
+```bash
+cd extension
+npm install
+```
+
+Go to chrome://extensions in your browser, then click "Load unpacked". Select the /extension folder.
+
+The next step is to add your OpenAI API key to your environment.
+```bash
+export OPENAI_KEY=<your key>
+```
+
+To run the Node.js server that sends requests to the OpenAI API:
+
+```bash
+cd server
+node app.js
+```
+
+At this point you should be able to launch the Chrome extension in your browser and start testing.
--- extension/main-vue.js
+++ extension/main-vue.js
@@ -1,5 +1,28 @@
 import { createApp, h } from "/node_modules/vue/dist/vue.esm-browser.js";
 
+/** @type Command[] */
+const commandHistory = [];
+
+const getCurrentTab = async () => {
+  try {
+    let [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: false,
+    });
+    return tab;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+const getLineById = (id, html) => {
+  const el = html.split("\n").find((line) => {
+    return line.replace(/^\s+/, "").startsWith(id);
+  });
+  return el || "";
+};
+
 const App = {
   name: 'App',
   data() {
@@ -11,7 +34,84 @@ const App = {
         h('img', { alt: 'BrowseGPT', src: 'logo.png', width: '163' }),
       ]),
       h('div', { class: 'main' }, [
-        h('form', { id: 'objective-form' }, [
+        h('form', { id: 'objective-form', onSubmit: async (e) => {
+          e.preventDefault();
+          // submitButton.setAttribute("disabled", true);
+          
+          const currentTab = await getCurrentTab();
+        
+          if (!currentTab) {
+            // errorEl.innerHTML =
+            //   "BrowseGPT extension could not access a tab. Try closing and re-launching the extension.";
+            return;
+          }
+        
+          const tabId = currentTab.id;
+          // errorEl.innerText = "";
+          // loadingEl.classList.remove("hidden");
+          let commandResponse = {};
+        
+          // const objective = objectiveInput.value;
+          const objective = e.target.objective.value;
+        
+          try {
+            /** @type BrowserContent */
+            const browserContent = await chrome.tabs.sendMessage(tabId, {
+              message: "get_browser_content",
+            });
+        
+            const { url, html } = browserContent;
+        
+            // pre.innerText = html;
+        
+            const previousSteps = commandHistory.map((c) => c.step);
+        
+            const body = JSON.stringify({
+              version: "2.0",
+              previousSteps,
+              url: url.substring(0, 50),
+              html,
+              objective,
+            });
+        
+            const res = await fetch("http://localhost:3000/command", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body,
+            });
+        
+            commandResponse = await res.json();
+            /** @type Command */
+            const command = JSON.parse(commandResponse.command);
+        
+            command.action.el = getLineById(command.action.id, html).trim();
+        
+            commandHistory.push(command);
+            if (commandHistory.length > 5) {
+              commandHistory.shift();
+            }
+            // renderHistory();
+        
+            chrome.tabs.sendMessage(tabId, {
+              message: "run_command",
+              command: commandResponse.command,
+            });
+          } catch (e) {
+            if (e.message.includes("Could not establish connection")) {
+              // errorEl.innerText =
+              //   "This page doesn't allow the BrowseGPT extension. Close the extension, go to a different URL like https://google.com, and then re-launch the extension from there.";
+            } else {
+              // errorEl.innerText = `Command: ${JSON.stringify(
+              //   commandResponse || ""
+              // )}\n Error: ${e.message}. Try closing and re-launching the extension.`;
+            }
+          } finally {
+            // loadingEl.classList.add("hidden");
+            // submitButton.removeAttribute("disabled");
+          }
+        }}, [
           h('label', { for: 'objective' }, 'Instruction'),
           h('br'),
           h('textarea', { id: 'objective' }),
